4d6fd21e24ad211920571552f7ff072ee8c88d6e
Spandan7724 <spandanchavan727477@gmail.com>
2024-07-18 00:53:26 +0530

    0.5.9

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="manufacture",
-    version="0.5.8",
+    version="0.5.9",
     packages=find_packages(),
     entry_points={
         'console_scripts': [
